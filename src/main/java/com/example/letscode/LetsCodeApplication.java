0295e9eeabc44b67d6988c5d29dd3198cef3739e
olegKvadratniy <sacha.margelova@gmail.com>
2023-03-29 21:10:19 +0300

    First commit

---
+++ src/main/java/com/example/letscode/LetsCodeApplication.java
@@ -0,0 +1,14 @@
+package com.example.letscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+LetsCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsCodeApplication.class, args);
+    }
+
+}
